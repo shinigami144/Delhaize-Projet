971ac0ac80743d174fc32b4661b9e34d7c6a67c5
Raphaël <r.muller@ludus-academie.com>
2020-10-20 15:59:27 +0200

    INSERTION dans la BDD  next step verifier que quelqu'un est dedans

--- insert.php
+++ insert.php
@@ -1,4 +1,6 @@
 <?php
+require_once('db.php'); 
+
 $nom = $_POST["nom"];
 $prenom = $_POST["prenom"];
 $mail1 = $_POST["mail1"];
@@ -10,10 +12,25 @@ $cp = $_POST["cp"];
 
 if(isset($nom) && isset($prenom) && isset($mail1) && isset($mail2) && isset($pass1) && isset($pass2) && isset($adresse) && isset($cp)){
     if(($mail1==$mail2)&&($pass1==$pass2)){
-        echo 'ok';
+        //inserer dans la db
+        try{
+            $sql = "insert into users(nom,prenom,email,pw,adresse,cp) values (:nom,:prenom,:email,:pw,:adresse,:cp)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":nom",$nom);
+            $stmt->bindParam(":prenom",$prenom);
+            $stmt->bindParam(":email",$mail1);
+            $stmt->bindParam(":pw",$pass1);
+            $stmt->bindParam(":adresse",$adresse);
+            $stmt->bindParam(":cp",$cp);
+            $stmt->execute();
+        } catch(PDOException $e){
+            echo "Error: " . $e->getMessage();
+        }
+        $pdo = null;
+        header("Location:index.php");
     }
     else {
-        headerLocation("index.php");
+        header("Location:index.php");
     }    
 }
 ?>
