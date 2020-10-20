ee9b5eab8bf70eb766f061a9939bbb32be297604
Raphaël <r.muller@ludus-academie.com>
2020-10-20 15:31:29 +0200

    ajout CSS

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
     <head>
         <title>Jeu Delhaize</title>
         <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css" />
     </head>
     <body>
         <?php
         require_once('db.php'); 
 	
+        /*
         $host = '127.0.0.1:3308';
         $db   = 'delhaize';
         $user = 'root';
@@ -22,35 +24,31 @@
             $pdo = new PDO($dsn, $user, $pass, $options);
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), (int)$e->getCode());
-        }
+        }*/
         //connection
         ?>
-        <table style="width:100%">
-            <tr>
-                <td>
-                    <h1>Inscription</h1>
-                    <form action="insert.php" method="POST">
-                        Nom : <input type="txt" name="nom" /> <br/>
-                        Prénom : <input type="txt" name="prenom" /> <br/>
-                        email : <input type="email" name="mail1"/>.<br/>
-			            confirmation email : <input type="email" name="mail2"/> <br/>
-                        mot de passe : <input type="password" name="pass1"/> <br/>
-                        confirmation mot de passe : <input type="password" name="pass2"/> <br/>
-			            adresse : <input type="txt" name="adresse" /> <br/>
-                        code postal : <input type="number" name="cp" /> <br/>
-                        <input type="submit"  value="envoyer"/>
-                    </form>
-                </td>
-                <td>
-                    <h1>Connection</h1>
-                    <form action="connection.php" method="POST">
-                        email : <input type="email" name="mail1"/><br/>
-                        mot de passe : <input type="password" name="pass1"/><br/><br/>
-                        <a href="#mpoublie">Mot de passe oublié</a><br/><br/>
-                        <input type="submit"  value="envoyer"/>
-                    </form>
-                </td>
-            </tr>
-        </table>
+       <div>
+            <h1>Inscription</h1>
+            <form action="insert.php" method="POST">
+                Nom : <input type="txt" name="nom" /> <br/>
+                Prénom : <input type="txt" name="prenom" /> <br/>
+                email : <input type="email" name="mail1"/>.<br/>
+                confirmation email : <input type="email" name="mail2"/> <br/>
+                mot de passe : <input type="password" name="pass1"/> <br/>
+                confirmation mot de passe : <input type="password" name="pass2"/> <br/>
+                adresse : <input type="txt" name="adresse" /> <br/>
+                code postal : <input type="number" name="cp" /> <br/>
+                <input type="submit"  value="envoyer"/>
+            </form>
+        </div>
+        <div>
+            <h1>Connection</h1>
+            <form action="connection.php" method="POST">
+                email : <input type="email" name="mail1"/><br/>
+                mot de passe : <input type="password" name="pass1"/><br/><br/>
+                <a href="#mpoublie">Mot de passe oublié</a><br/><br/>
+                <input type="submit"  value="envoyer"/>
+            </form>
+        </div>
     </body>
 </html>
