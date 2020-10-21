0e70fbc28f71df43d53381aa726dd521a927ba1f
Raphaël <r.muller@ludus-academie.com>
2020-10-21 10:28:46 +0200

    style + inscription vers BD

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
     <head>
         <title>Jeu Delhaize</title>
         <meta charset="utf-8"/>
-        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="style.css" >
     </head>
     <body>
         <?php
         require_once('db.php'); 
-	
+        session_start();
+        
+        if (isset($_SESSION['erreur'])){echo "<p><span style='color: red;
+font-size: xxx-large;
+margin-left: 38%; '>".$_SESSION['erreur']."</span></p>";};
         /*
         $host = '127.0.0.1:3308';
         $db   = 'delhaize';
@@ -28,26 +32,12 @@
         //connection
         ?>
        <div>
-            <h1>Inscription</h1>
+            <h1 style="font-size:34px;text-decoration: underline;">Inscription</h1>
             <form action="insert.php" method="POST">
-                Nom : <input type="txt" name="nom" /> <br/>
-                Prénom : <input type="txt" name="prenom" /> <br/>
-                email : <input type="email" name="mail1"/>.<br/>
+                <p style="font-size:20px;">email : <input type="email" name="mail1"/>.<br/>
                 confirmation email : <input type="email" name="mail2"/> <br/>
-                mot de passe : <input type="password" name="pass1"/> <br/>
-                confirmation mot de passe : <input type="password" name="pass2"/> <br/>
-                adresse : <input type="txt" name="adresse" /> <br/>
-                code postal : <input type="number" name="cp" /> <br/>
-                <input type="submit"  value="envoyer"/>
-            </form>
-        </div>
-        <div>
-            <h1>Connection</h1>
-            <form action="connection.php" method="POST">
-                email : <input type="email" name="mail1"/><br/>
-                mot de passe : <input type="password" name="pass1"/><br/><br/>
-                <a href="#mpoublie">Mot de passe oublié</a><br/><br/>
-                <input type="submit"  value="envoyer"/>
+                code : <input type="txt" name="code" /> <br/>
+                    <input type="submit"  value="envoyer"/></p>
             </form>
         </div>
     </body>
--- insert.php
+++ insert.php
@@ -1,27 +1,19 @@
 <?php
 require_once('db.php'); 
+session_start();
 
-$nom = $_POST["nom"];
-$prenom = $_POST["prenom"];
 $mail1 = $_POST["mail1"];
 $mail2 = $_POST["mail2"];
-$pass1 = $_POST["pass1"];
-$pass2 = $_POST["pass2"];
-$adresse = $_POST["adresse"];
-$cp = $_POST["cp"];
+$code = $_POST["code"];
 
-if(isset($nom) && isset($prenom) && isset($mail1) && isset($mail2) && isset($pass1) && isset($pass2) && isset($adresse) && isset($cp)){
-    if(($mail1==$mail2)&&($pass1==$pass2)){
+if(isset($mail1)&&isset($mail2)&&isset($code)){
+    if(($mail1==$mail2)){
         //inserer dans la db
         try{
-            $sql = "insert into users(nom,prenom,email,pw,adresse,cp) values (:nom,:prenom,:email,:pw,:adresse,:cp)";
+            $sql = "insert into users(code,email) values (:code,:email)";
             $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(":nom",$nom);
-            $stmt->bindParam(":prenom",$prenom);
             $stmt->bindParam(":email",$mail1);
-            $stmt->bindParam(":pw",$pass1);
-            $stmt->bindParam(":adresse",$adresse);
-            $stmt->bindParam(":cp",$cp);
+            $stmt->bindParam(":code",$code);
             $stmt->execute();
         } catch(PDOException $e){
             echo "Error: " . $e->getMessage();
@@ -30,6 +22,7 @@ if(isset($nom) && isset($prenom) && isset($mail1) && isset($mail2) && isset($pas
         header("Location:index.php");
     }
     else {
+        $_SESSION['erreur']="Mauvais e-Mail!";
         header("Location:index.php");
     }    
 }
