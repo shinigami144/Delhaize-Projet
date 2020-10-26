ac26f6af1861fa87c7b2e1547fbbbeb9fa7f6b34
Raphaël <r.muller@ludus-academie.com>
2020-10-26 11:41:09 +0100

    suppression de tout les choses inutiles

---
+++ inscription.php
@@ -0,0 +1,48 @@
+<html>
+    <head>
+        <title>Jeu Delhaize</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css" >
+    </head>
+    <body>
+        <script>
+        document.body.requestFullscreen();
+        </script>
+        <?php
+        require_once('db.php'); 
+        session_start();
+        
+        if (isset($_SESSION['erreur'])){echo "<p><span style='color: red;
+font-size: xxx-large;
+margin-left: 38%; '>".$_SESSION['erreur']."</span></p>";};
+        /*
+        $host = '127.0.0.1:3308';
+        $db   = 'delhaize';
+        $user = 'root';
+        $pass = '';
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $dsn = "mysql:host=$host;dbname=$db";
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }*/
+        //connection
+        ?>
+       <div>
+            <h1 style="font-size:34px;text-decoration: underline;">Inscription</h1>
+            <form action="web_promo_noel_2020" app-token="XLGFHEKSPFJTHEPPPA"  method="POST">
+                <p style="font-size:20px;">
+                email : <input type="email" name="user-email"/><br/>
+                ticket-id : <input type="txt" name="ticket-id" /> <br/>
+                ticket-time : <input type="number" name="ticket-time" /> <br/>
+                    <input type="submit"  value="envoyer"/></p>
+            </form>
+        </div>
+    </body>
+</html>
