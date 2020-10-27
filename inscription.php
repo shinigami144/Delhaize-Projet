e76bc40d373ae2560c8e610d261d415f31bf527c
Raphaël <r.muller@ludus-academie.com>
2020-10-27 12:00:56 +0100

    réusi envoi vers la page elle meme mais vide xD

--- inscription.php
+++ inscription.php
@@ -6,30 +6,25 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
-        
         <?php
         session_start();
-        
-        
         ?>
        <div>
             <h1 style="font-size:34px;text-decoration: underline;">Inscription</h1>
             <form action="#web_promo_noel_2020" app-token="XLGFHEKSPFJTHEPPPA"  method="POST">
                 <p style="font-size:20px;">
-                email : <input type="email" name="user-email" value="test@test.test"/><br/>
-                ticket-id : <input type="txt" name="ticket-id"  value="1"/> <br/>
-                ticket-time : <input type="number" name="ticket-time" value="2" /> <br/>
+                email : <input type="email" name="user-email" id='mail'/><br/>
+                ticket-id : <input type="txt" name="ticket-id" id='ticketid'/> <br/>
+                ticket-time : <input type="number" name="ticket-time" id='tickettime' /> <br/>
                     <input type="submit"  value="envoyer"/></p>
             </form>
         </div>
         <script>
         var data = new FormData();
         data.append('app-token', 'XLGFHEKSPFJTHEPPPA');
-        data.append('user-email', 'test@test.test');
-        data.append('ticket-id', '1');
-        data.append('ticket-time', '2');
-       
-
+        data.append('user-email', document.getElementById('mail').value);
+        data.append('ticket-id', document.getElementsByName("ticketid").value);
+        data.append('ticket-time', document.getElementsByName("tickettime").value);
         var xhr = new XMLHttpRequest();
         xhr.open('POST', "c'est au client de regarder !il avait qu'a nous donner le lien ver l'api mais bon il peut pparament le faire lui meme ...", true);
         xhr.onload = function () {
