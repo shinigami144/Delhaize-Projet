16971f13340dd7ebe49381ec037acf736dfadc61
Raphaël <r.muller@ludus-academie.com>
2020-10-28 11:29:58 +0100

    partage FB + Twitter on s'en fiche d'instagram car impossible

--- partageFB.php
+++ partageFB.php
@@ -7,8 +7,8 @@
         <meta property="og:image"         content="https://www.journaldugeek.com/content/uploads/2017/04/reine-des-neiges-snowman.jpg" />
         
     </head>
-    <body>
-        <a target="_top" href="https://www.facebook.com/sharer/sharer.php?u=http://delhaize.be">Share on Facebook</a>
-        <script></script>
+    <body style="background-color : grey;">
+        <a target="_top" href="https://www.facebook.com/sharer/sharer.php?u=http://delhaize.be" style="background-color : grey; font-size : 30; color : white;">Share on Facebook</a>
+        <a href="https://twitter.com/intent/tweet?button_hashtag=share&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-size="large" data-text="I played the delhaize Frozen II Game" data-url="http://delhaize-projet/partageFB.php" data-related="Delhaize,Ludus" data-dnt="true" data-show-count="false">Tweet #share</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </body>
 </html>
