6985f3d605ce77e1b9c68a9f595cd98b355bea86
Raphaël <r.muller@ludus-academie.com>
2020-11-02 17:16:29 +0100

    avec la possiblilité de fixer le texte pour facebook en comenté car nécesite un app token

--- partageFB.php
+++ partageFB.php
@@ -24,6 +24,22 @@ $score=$_POST['score'];
                         <div class="fb-share-button" data-href="http://www.delhaize.be" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Partager</a></div>
                         <a href="https://twitter.com/intent/tweet?button_hashtag=share&ref_src=twsrc%5Etfw" class="twitter-hashtag-button" data-size="large" data-text="I played the delhaize Frozen II Game My score was '.$score.'" data-url="http://delhaize-projet/partageFB.php" data-related="Delhaize,Ludus" data-dnt="true" data-show-count="false">Tweet #share</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                         ';
+                    
+                    //message facebook pour les applications avec canvas -> besoin de app token pour etre fonctionel -> moyen de preset le message
+                    /*<div id="requestsBtn" class="btn btn-success clearfix">Basic Request Dialog</div>
+
+                    <script>
+                    document.getElementById('requestsBtn').onclick = function() {
+                        FB.ui({method: 'apprequests',
+                            message: 'This is a test message for the requests dialog.'
+                            }, function(data) {
+                            Log.info('App Requests Response', data);
+                            }
+                        );
+                    }
+                    </script>
+
+                    */
                     }
                 else{
                     if($_SESSION['lan']== 'en')
