deaf1535ea2e5f27731bd0612744f23f3a7ad6bb
Shini <d.abraham@ludus-academie.com>
2020-11-03 19:45:11 +0100

    Mise en place d'une interface comme demander par Graphique

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
         session_start();
         ?>
         <form action="setlanguage.php" method="post">
-            <div><button type="submit" name="langue" value='en'><img src="img/en.jpg" alt="English"/></button></div>
-            <div><button type="submit" name="langue" value='fr'><img src="img/fr.png" alt="French"/></button></div>
-            <div><button type="submit" name="langue" value='nl'><img src="img/NL.jpg" alt="Dutch"/></button></div>
+            
+            <div id="Group">
+                <div class="Contenu"><button type="submit" name="langue" value='en'><img src="img/en.jpg" alt="English"/></button></div>
+                <div class="Contenu"><button type="submit" name="langue" value='fr'><img src="img/fr.png" alt="French"/></button></div>
+                <div class="Contenu"><button type="submit" name="langue" value='nl'><img src="img/NL.jpg" alt="Dutch"/></button></div>
+            </div>
         </form>
     </body>
 </html>
