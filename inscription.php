08a7899b79c07186655c9ab45477e0e6d9abb0d2
Raphaël <r.muller@ludus-academie.com>
2020-11-03 18:28:48 +0100

    mauvaise langues au mauvais androit fix

--- inscription.php
+++ inscription.php
@@ -39,20 +39,20 @@ session_start();
     <body style="background: linear-gradient(#472234, #1A364B);background-image: url('/Asset/Graphique/AssetsSiteWeb/page_inscription/background_page_inscription_PNG8.png');background-size: 100% 100%;">
         <img src="Asset/Graphique/AssetsSiteWeb/page_inscription/logo_frozen2_eng.png" /><img src="Asset/Graphique/AssetsSiteWeb/page_inscription/texte_inscrivez_vous_eng_PNG8.png"/ style="margin-left: 45%;margin-top:-15%;">
         <?php 
-        if($_SESSION['lan']=='en'){
+        if($_SESSION['lan']=='nl'){
             echo '<form action="envoie.php" app-token="XLGFHEKSPFJTHEPPPA" method="POST" class="inscription_main" style="display:block;" id="form">
             <div class="inscription_info">
-                <span class="surlignetexte">E-MAIL</span> <input style="width: 75%;height: 60%;" type="email" name="user-email" id="mai" id="fname"/>
+                <span class="surlignetexte">E-MAIL</span> <input style="width: 75%;height: 60%;" type="email" name="user-email" id="mail" id="fname"/>
             </div>
             <div class="inscription_info">
 				<span class="surlignetexte">TICKET NUMBER</span> <input style="width: 60%;height: 60%;" type="txt" name="ticket-id" id="ticketid" id="fname"/>
             </div>
             <div class="inscription_info">
-				<span class="surlignetexte">TICKET TIME</span> <input style="height: 60%;width:65%;" type="number" name="ticket-time" id="tickettime" id="fname"/>
+				<span class="surlignetexte">TICKET TIJD</span> <input style="height: 60%;width:65%;" type="number" name="ticket-time" id="tickettime" id="fname"/>
             </div>
             <input type="image" form="form" style="margin-top: 10%;
             margin-left: 20%;" src="./Asset/Graphique/AssetsSiteWeb/page_inscription/bouton_envoyer_eng_PNG8.png" value="envoyer"/>
-        </form>';
+        </form>';    
         }else{if($_SESSION['lan']=='nl'){
             echo '<form action="envoie.php" app-token="XLGFHEKSPFJTHEPPPA" method="POST" class="inscription_main" style="display:block;" id="form">
             <div class="inscription_info">
@@ -67,18 +67,17 @@ session_start();
             <input type="image" form="form" style="margin-top: 10%;
             margin-left: 20%;" src="./Asset/Graphique/AssetsSiteWeb/page_inscription/bouton_envoyer_eng_PNG8.png" value="envoyer"/>
         </form>';
-        }
-            
+        }    
         else{
             echo '<form action="envoie.php" app-token="XLGFHEKSPFJTHEPPPA" method="POST" class="inscription_main" style="display:block;" id="form">
             <div class="inscription_info">
-                <span class="surlignetexte">E-MAIL</span> <input style="width: 75%;height: 60%;" type="email" name="user-email" id="mail" id="fname"/>
+                <span class="surlignetexte">E-MAIL</span> <input style="width: 75%;height: 60%;" type="email" name="user-email" id="mai" id="fname"/>
             </div>
             <div class="inscription_info">
 				<span class="surlignetexte">TICKET NUMBER</span> <input style="width: 60%;height: 60%;" type="txt" name="ticket-id" id="ticketid" id="fname"/>
             </div>
             <div class="inscription_info">
-				<span class="surlignetexte">TICKET TIJD</span> <input style="height: 60%;width:65%;" type="number" name="ticket-time" id="tickettime" id="fname"/>
+				<span class="surlignetexte">TICKET TIME</span> <input style="height: 60%;width:65%;" type="number" name="ticket-time" id="tickettime" id="fname"/>
             </div>
             <input type="image" form="form" style="margin-top: 10%;
             margin-left: 20%;" src="./Asset/Graphique/AssetsSiteWeb/page_inscription/bouton_envoyer_eng_PNG8.png" value="envoyer"/>
